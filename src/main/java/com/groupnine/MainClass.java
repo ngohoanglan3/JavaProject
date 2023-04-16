240a78cd72128ab1a000cf6558ab78e6316158b6
Mrsteve <92042954+ngohoanglan3@users.noreply.github.com>
2023-04-16 15:31:37 +0700

    1

---
+++ src/main/java/com/groupnine/MainClass.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.groupnine;
+import com.groupnine.ui.StudentLoginFrame;
+
+/**
+ *
+ * @author ngohoanglan2k3
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+        StudentLoginFrame frame = new StudentLoginFrame();
+        frame.setVisible(true);
+    }
+}
